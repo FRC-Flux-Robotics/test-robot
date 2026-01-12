8578d464f004e95829bcbb06339e397013f3e94c
Tomas Tvaroska <tomas@tvaroska.com>
2026-01-12 00:46:21 +0000

    Remove commented dead code from Sensitivity.java Removed 48 lines of unused debug and alternative implementation code

--- src/main/java/frc/robot/Sensitivity.java
+++ src/main/java/frc/robot/Sensitivity.java
@@ -39,19 +39,13 @@ class Sensitivity {
           return 0;
 
         if (xabs <= cuspX) {
-            //System.out.println("Lin " + m_linCoef + " x: " + x);
             xabs *= linCoef;
         }
         else {
-            //if (xabs <= 1.0) {
-            //System.out.println("Quad " + m_threshold + " x: " + x);
             xabs = a * xabs * xabs + b * xabs + c;
         }
         if (xabs > limit)
             xabs = limit;
-        // if (xabs < minLimit)
-        //     xabs = limit;
-// /        System.out.println("SENS in: " + x + " out: " + xabs);
         return x >= 0 ? xabs : -xabs;
     }
 
@@ -61,44 +55,3 @@ class Sensitivity {
       return abs > 0 && abs < minValue ? sign * minValue : value;
     }
 }
-
-/*
-double sensitivity(double x) {
-    //m_speedLimit1 = SmartDashboard.getNumber("Drive speed limit1", 0.1);
-
-    double xabs = Math.abs(x);
-    if (xabs <= m_threshold) {
-      return m_linCoef * x;
-    }
-    else {//if (xabs <= 1.0) {
-      // double x2 = (xabs - m_speedStep1);
-      // x2 *= x2;
-
-      double c = m_linCoef * m_cuspX;
-      double coef = (1 - c) / (1 - m_threshold);
-      coef *= m_speedLimitX;
-      xabs = coef * (xabs - m_threshold) + c;
-      x = x >= 0 ? xabs : -xabs;
-    }
-    // else if (xabs <= 0.6) {
-    //   // 0.32 = 0.4 * k
-    //   //x = 0.4 * (x - 0.2) + 0.04;
-    // }
-    // else {
-    //   // 0.2
-    //   //x = m_speedLimit1 * (x - 0.6) + 0.2;
-    // }
-    return x;
-  }
-
-  static double sensitivity3(double x) {
-    double xabs = Math.abs(x);
-    if (xabs < 0.1)
-      return 0;
-    else if (xabs < 0.6)
-      return 0.4 * Math.signum(x) * (xabs);
-    else return 0;
-    //return 0.5 * x * x * x;
-  }
-}
-*/
